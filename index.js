0a304e43599c111ed046582b23870228a95c207b
savagem7 <i0g1n4oj>
2021-07-03 17:39:55 +0100

    new smtp

--- index.js
+++ index.js
@@ -1,52 +1,68 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-const cors = require("cors");
+const multiparty = require("multiparty");
+require("dotenv").config();
 
+// instantiate an express app
 const app = express();
+var cors = require('cors');
+app.use(cors({origin: true, credentials: true}));
 
-const port = 4444;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(cors());
-
-app.listen(process.env.PORT || port, () => {
-    console.log("We are live on port 4444");
-});
-
-app.get("/", (req, res) => {
+//make the contact page the the first page on the app
+app.route("/").get(function (req, res) {
     res.send("Welcome to my mail api");
 });
 
-app.post("/api/v1", (req, res) => {
-    let data = req.body;
+//port will be 5000 for testing
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+});
 
-    let smtpTransport = nodemailer.createTransport({
-        service: "hotmail",
-        auth: {
+const transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com", //replace with your email provider
+    port: 587,
+    auth: {
         user: "michaelsavage119@hotmail.com",
         pass: "03F12qlZ",
-        },
-    });
+    },
+});
 
-    console.log(data);
+// verify connection configuration
+transporter.verify(function (error, success) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Server is ready to take our messages");
+    }
+});
 
-    let mailOptions = {
-        from: data.email,
-        to: "michaelsavage119@hotmail.com",
-        subject: `${data.subject}`,
-        html: `<p>${data.name}</p>
-            <p>${data.email}</p>
-            <p>${data.message}</p>`,
-    };
+app.post("/send", (req, res) => {
+    //1.
+    let form = new multiparty.Form();
+    let data = {};
+    form.parse(req, function (err, fields) {
+        console.log(fields);
+        Object.keys(fields).forEach(function (property) {
+        data[property] = fields[property].toString();
+        });
 
-    smtpTransport.sendMail(mailOptions, (error, response) => {
-        if (error) {
-            res.send(error);
-        } else {
-            res.send("Success");
-        }
-        smtpTransport.close();
+        //2. You can configure the object however you want
+        const mail = {
+            from: data.name,
+            to: process.env.EMAIL,
+            subject: data.subject,
+            text: `${data.name} <${data.email}> \n${data.message}`,
+        };
+
+        //3.
+        transporter.sendMail(mail, (er, data) => {
+            if (er) {
+            console.log(er);
+            res.status(500).send("Something went wrong.");
+            } else {
+            res.status(200).send("Email successfully sent to recipient!");
+            }
+        });
     });
 });
