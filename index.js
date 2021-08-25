50fa5f88ea790ae34b8a73cd1377a1244abdb752
savagem7 <i0g1n4oj>
2021-08-25 22:48:41 +0100

    updating hotmail and adding env

--- index.js
+++ index.js
@@ -1,68 +1,46 @@
-const express = require("express");
-const nodemailer = require("nodemailer");
-const multiparty = require("multiparty");
 require("dotenv").config();
-
-// instantiate an express app
+const express = require("express");
 const app = express();
+const nodemailer = require("nodemailer");
+
 var cors = require('cors');
-app.use(cors({origin: true, credentials: true}));
+app.use(cors());
+app.use(express.json());
 
-//make the contact page the the first page on the app
-app.route("/").get(function (req, res) {
-    res.send("Welcome to my mail api");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}...`);
 });
 
-//port will be 5000 for testing
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
-});
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
 
-const transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com", //replace with your email provider
-    port: 587,
-    auth: {
-        user: "michaelsavage119@hotmail.com",
-        pass: "03F12qlZ",
-    },
-});
+app.post("/", async (req, res) => {
+    console.log(req.body.name);
+    const transporter = nodemailer.createTransport({
+        host: "smtp.live.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+        },
+    });
 
-// verify connection configuration
-transporter.verify(function (error, success) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Server is ready to take our messages");
+    const mailOptions = {
+        from: process.env.EMAIL,
+        to: process.env.EMAIL,
+        subject: `Message on thatssavage.ie: ${req.body.email}`,
+        text: req.body.message + ",\n" + req.body.name 
     }
-});
 
-app.post("/send", (req, res) => {
-    //1.
-    let form = new multiparty.Form();
-    let data = {};
-    form.parse(req, function (err, fields) {
-        console.log(fields);
-        Object.keys(fields).forEach(function (property) {
-        data[property] = fields[property].toString();
-        });
-
-        //2. You can configure the object however you want
-        const mail = {
-            from: data.name,
-            to: process.env.EMAIL,
-            subject: data.subject,
-            text: `${data.name} <${data.email}> \n${data.message}`,
-        };
-
-        //3.
-        transporter.sendMail(mail, (er, data) => {
-            if (er) {
-            console.log(er);
-            res.status(500).send("Something went wrong.");
-            } else {
-            res.status(200).send("Email successfully sent to recipient!");
-            }
-        });
+    await transporter.sendMail(mailOptions, (error, info) => {
+        if(error) {
+            res.send("error");
+        } else {
+            console.log("Email Sent!");
+            res.send("success");
+        }
     });
 });
